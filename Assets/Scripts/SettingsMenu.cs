d01f0600d56fcc116c9a06400e63d401148fd2ac
FerhatSezgin <ziyaretli.ferhat.56@gmail.com>
2025-11-05 18:11:28 +0300

    MainMenu düzenleme

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -3,35 +3,35 @@ using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    [Header("UI References")]
     public GameObject settingsPanel;
+    public GameObject mainMenuPanel;
     public Toggle soundToggle;
-    public Toggle musicToggle;
 
     private void Start()
     {
-
+        // Oyun başladığında Settings Panel kapalı olsun
         settingsPanel.SetActive(false);
     }
 
     public void OpenSettings()
     {
         settingsPanel.SetActive(true);
+        if (mainMenuPanel != null)
+            mainMenuPanel.SetActive(false);
     }
 
     public void CloseSettings()
     {
         settingsPanel.SetActive(false);
+        if (mainMenuPanel != null)
+            mainMenuPanel.SetActive(true);
     }
 
     public void ToggleSound()
     {
         Debug.Log("Sound: " + soundToggle.isOn);
-        // Buraya ses sistemin varsa bağlayabilirsin.
-    }
-
-    public void ToggleMusic()
-    {
-        Debug.Log("Music: " + musicToggle.isOn);
-        // Buraya müzik sistemi bağlanacak. ddd
+        // Buraya ileride ses sistemini bağlayabilirsin
+        // Örn: AudioListener.volume = soundToggle.isOn ? 1 : 0;
     }
 }
